c38bc5be49635cc6f1250c130170ff7d0e5099ba
Astone345 <astone345@gmail.com>
2018-04-25 09:01:03 -0500

    edit for testing purposes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,7 +3,7 @@
  */
 public class App {
     public String getGreeting() {
-        return "Hello world.";
+        return "Hello world. I have been changed.";
     }
 
     public static void main(String[] args) {
